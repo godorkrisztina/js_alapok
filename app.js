8116d00159bf3d1745df99e7325c23b10b64ef71
Unknown <g.krisztina97@gmail.com>
2018-02-16 09:30:33 +0100

    gallery

--- app.js
+++ app.js
@@ -2,10 +2,24 @@
 /*global $, jQuery, alert*/
 /*jslint indent: 2 */
 
-
-console.log("hello world");
-
+(function() {
+    'use strict';
+    console.log("hello world");
 $('.image-popup').magnificPopup({
-    type: 'image'
-    /*gallery: { enabled: true }*/
+    type: 'image',
+    gallery: { 
+        enabled: true 
+    }
+    });  
+}());
+
+
+/*minden image-popup class-al rendelkező elemet elrejtjük */
+/*sima javascriptet használunk ehhez:*/
+   [].forEach.call(document.querySelectorAll('.image-popup'),
+    function(el) {
+    el.style.display = 'none';
 });
+
+/*Ez ugyanazt csinálj amint a fentibb kód, csak ez a jquery library segítségével:*/
+$('.image-popup').css('display', 'none');
